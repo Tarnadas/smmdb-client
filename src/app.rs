1cd533d857597a43aaf66b7af8b3536b8747178a
Mario Reder <mreder1289@gmail.com>
2020-08-07 08:02:58 +0200

    error messages

--- src/app.rs
+++ src/app.rs
@@ -48,35 +48,39 @@ impl Application for App {
                 async move { smmdb::Save::new(save.get_location().clone()).await },
                 |res| match res {
                     Ok(save) => Message::LoadSave(save),
-                    Err(err) => Message::LoadSaveError("".to_string()), // TODO
+                    Err(err) => Message::LoadSaveError(err.into()),
                 },
             ),
             Message::OpenCustomSave => {
-                let result = nfd::open_pick_folder(None).unwrap_or_else(|e| {
-                    panic!(e);
-                });
-
-                match result {
-                    Response::Okay(file_path) => {
-                        let file_path: PathBuf = file_path.into();
-                        if is_yuzu_dir(file_path.clone()) {
-                            self.save_buttons
-                                .insert(0, Box::new(SaveButton::new(file_path, EmuType::Yuzu)));
-                        } else if is_ryujinx_dir(file_path.clone()) {
-                            self.save_buttons
-                                .insert(0, Box::new(SaveButton::new(file_path, EmuType::Ryujinx)));
+                match nfd::open_pick_folder(None) {
+                    Ok(result) => match result {
+                        Response::Okay(file_path) => {
+                            let file_path: PathBuf = file_path.into();
+                            if is_yuzu_dir(file_path.clone()) {
+                                self.save_buttons
+                                    .insert(0, Box::new(SaveButton::new(file_path, EmuType::Yuzu)));
+                            } else if is_ryujinx_dir(file_path.clone()) {
+                                self.save_buttons.insert(
+                                    0,
+                                    Box::new(SaveButton::new(file_path, EmuType::Ryujinx)),
+                                );
+                            }
+                            Command::none()
                         }
-                        Command::none()
-                    }
-                    Response::OkayMultiple(_files) => {
-                        println!("Not multifile select");
-                        Command::none()
-                    }
-                    Response::Cancel => {
-                        println!("User canceled");
-                        Command::none()
-                    }
+                        Response::OkayMultiple(_files) => {
+                            println!("Not multifile select");
+                            Command::none()
+                        }
+                        Response::Cancel => {
+                            println!("User canceled");
+                            Command::none()
+                        }
+                    },
+                    Err(err) => Command::perform(async {}, move |_| {
+                        Message::LoadSaveError(format!("{:?}", err))
+                    }),
                 }
+                // .unwrap_or_else(|err| Message::LoadSaveError(err.into()));
             }
             Message::LoadSave(save) => {
                 self.save = Some(save);
